77943e6f50aa87846e8caccfaa82c5ed0fa0ec06
Firoz Uddin <124498500+firozuddingauhar@users.noreply.github.com>
2024-04-07 23:27:31 +0530

    Add files via upload

---
+++ snipets/word_distance.py
@@ -0,0 +1,19 @@
+def wagner_fischer(s1, s2):
+    len_s1, len_s2 = len(s1), len(s2)
+    if len_s1 > len_s2:
+        s1, s2 = s2, s1
+        len_s1, len_s2 = len_s2, len_s1
+
+    current_row = range(len_s1 + 1)
+    for i in range(1, len_s2 + 1):
+        previous_row, current_row = current_row, [i] + [0] * len_s1
+        for j in range(1, len_s1 + 1):
+            add, delete, change = previous_row[j] + 1, current_row[j-1] + 1, previous_row[j-1]
+            if s1[j-1] != s2[i-1]:
+                change += 1
+            current_row[j] = min(add, delete, change)
+
+    return current_row[len_s1]
+
+s1, s2 = input("enter two words: ").split()
+print("word distance between them is: ",wagner_fischer(s1, s2))
